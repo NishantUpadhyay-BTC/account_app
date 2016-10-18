bcd39b2322481eaf0767128bc6ec8adbd524f410
NishantUpadhyay-BTC <nishantupadhyay@botreeconsulting.com>
2016-10-18 12:28:56 +0530

    create toggle form for edit action in table of records

--- app/assets/javascripts/components/record.es6.jsx
+++ app/assets/javascripts/components/record.es6.jsx
@@ -1,7 +1,20 @@
 class Record extends React.Component {
   constructor(){
-    super()
+    super();
+    this.state = { edit: false};
     this.handleDelete = this.handleDelete.bind(this);
+    this.handleToggle = this.handleToggle.bind(this);
+    this.recordRow = this.recordRow.bind(this);
+    this.handleEdit = this.handleEdit.bind(this);
+  }
+
+  handleToggle(e){
+    e.preventDefault();
+    this.setState({edit: !(this.state.edit)});
+  }
+
+  handleEdit(e){
+    e.preventDefault();
   }
 
   handleDelete(e){
@@ -20,14 +33,39 @@ class Record extends React.Component {
    });
   }
 
-  render () {
+  recordRow(){
     return(
       <tr key={this.props.data}>
         <td>{this.props.data.date}</td>
         <td>{this.props.data.title}</td>
         <td>{amountFormat(this.props.data.amount)}</td>
-        <td><a href="javascript:;" className="btn btn-danger" onClick={this.handleDelete}>Delete</a></td>
+        <td>
+          <a href="javascript:;" className="btn btn-default" onClick={this.handleToggle}>Edit</a>
+          <a href="javascript:;" className="btn btn-danger" onClick={this.handleDelete}>Delete</a>
+        </td>
       </tr>
     );
+  }
+
+  recordForm(){
+    return(
+      <tr key={this.props.data}>
+        <td><input type="text" className="form-control" defaultValue={this.props.data.date} ref="date" /></td>
+        <td><input type="text" className="form-control" defaultValue={this.props.data.title} ref="title"/></td>
+        <td><input type="text" className="form-control" defaultValue={this.props.data.amount} /></td>
+        <td>
+          <a href="javascript:;" className="btn btn-default" onClick={this.handleEdit}>Edit</a>
+          <a href="javascript:;" className="btn btn-danger" onClick={this.handleToggle}>Cancel</a>
+        </td>
+      </tr>
+    );
+  }
+
+  render(){
+    if (this.state.edit) {
+      return this.recordForm();
+    } else {
+      return this.recordRow();
+    }
   }
 }
